75147b40a6954efc8a5155bf63f34258ec238e2c
Lumien <marian.kreiser@googlemail.com>
2018-04-06 23:21:28 +0200

    Fixed #296 & Fixed #295: Update JEI Integration

--- src/main/java/lumien/randomthings/handler/compability/jei/RandomThingsPlugin.java
+++ src/main/java/lumien/randomthings/handler/compability/jei/RandomThingsPlugin.java
@@ -1,11 +1,17 @@
 package lumien.randomthings.handler.compability.jei;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
+import lumien.randomthings.block.ModBlocks;
 import lumien.randomthings.client.gui.GuiImbuingStation;
 import lumien.randomthings.container.ContainerImbuingStation;
-import lumien.randomthings.handler.compability.jei.anvil.AnvilRecipeCategory;
-import lumien.randomthings.handler.compability.jei.anvil.AnvilRecipeHandler;
 import lumien.randomthings.handler.compability.jei.imbuing.ImbuingRecipeCategory;
-import lumien.randomthings.handler.compability.jei.imbuing.ImbuingRecipeHandler;
+import lumien.randomthings.handler.compability.jei.imbuing.ImbuingRecipeWrapper;
+import lumien.randomthings.recipes.anvil.AnvilRecipe;
+import lumien.randomthings.recipes.imbuing.ImbuingRecipe;
 import mezz.jei.api.IGuiHelper;
 import mezz.jei.api.IJeiHelpers;
 import mezz.jei.api.IJeiRuntime;
@@ -14,9 +20,12 @@ import mezz.jei.api.IModRegistry;
 import mezz.jei.api.ISubtypeRegistry;
 import mezz.jei.api.JEIPlugin;
 import mezz.jei.api.ingredients.IModIngredientRegistration;
+import mezz.jei.api.recipe.IRecipeCategoryRegistration;
+import mezz.jei.api.recipe.IRecipeWrapper;
 import mezz.jei.api.recipe.IStackHelper;
+import mezz.jei.api.recipe.VanillaRecipeCategoryUid;
 import mezz.jei.api.recipe.transfer.IRecipeTransferRegistry;
-import net.minecraft.inventory.ContainerRepair;
+import net.minecraft.item.ItemStack;
 
 @JEIPlugin
 public class RandomThingsPlugin implements IModPlugin
@@ -25,7 +34,6 @@ public class RandomThingsPlugin implements IModPlugin
 	public static IStackHelper stackHelper;
 
 	public static String IMBUE_ID = "Imbuing";
-	public static String ANVIL_ID = "AnvilCraftingRT";
 
 	@Override
 	public void register(IModRegistry registry)
@@ -34,21 +42,33 @@ public class RandomThingsPlugin implements IModPlugin
 		RandomThingsPlugin.stackHelper = jeiHelpers.getStackHelper();
 		
 		IGuiHelper guiHelper = jeiHelpers.getGuiHelper();
-		registry.addRecipeCategories(new ImbuingRecipeCategory(guiHelper), new AnvilRecipeCategory(guiHelper));
 
-		registry.addRecipeHandlers(new ImbuingRecipeHandler(), new AnvilRecipeHandler());
-
+		registry.handleRecipes(ImbuingRecipe.class, (recipe) -> (new ImbuingRecipeWrapper(recipe.getIngredients(), recipe.toImbue(), recipe.getResult())), IMBUE_ID);
+		registry.addRecipeCatalyst(new ItemStack(ModBlocks.imbuingStation), IMBUE_ID);
+		
 		IRecipeTransferRegistry recipeTransferRegistry = registry.getRecipeTransferRegistry();
 
 		recipeTransferRegistry.addRecipeTransferHandler(ContainerImbuingStation.class, IMBUE_ID, 0, 4, 5, 36);
-		recipeTransferRegistry.addRecipeTransferHandler(ContainerRepair.class, ANVIL_ID, 0, 2, 3, 36);
 
-		registry.addRecipes(lumien.randomthings.recipes.imbuing.ImbuingRecipeHandler.imbuingRecipes);
-		registry.addRecipes(lumien.randomthings.recipes.anvil.AnvilRecipeHandler.getAllRecipes());
+		registry.addRecipes(lumien.randomthings.recipes.imbuing.ImbuingRecipeHandler.imbuingRecipes, IMBUE_ID);
+		
+		List<IRecipeWrapper> anvilRecipes = new ArrayList<IRecipeWrapper>();
+		for (AnvilRecipe ar:lumien.randomthings.recipes.anvil.AnvilRecipeHandler.getAllRecipes())
+		{
+			anvilRecipes.add(jeiHelpers.getVanillaRecipeFactory().createAnvilRecipe(ar.getFirst(), Lists.newArrayList(ar.getSecond()), Lists.newArrayList(ar.getOutput())));
+		}
+		
+		registry.addRecipes(anvilRecipes, VanillaRecipeCategoryUid.ANVIL);
 
 		registry.addRecipeClickArea(GuiImbuingStation.class, 99, 54, 22, 16, IMBUE_ID);
 		
 		DescriptionHandler.addDescriptions(registry);
+	}
+	
+	@Override
+	public void registerCategories(IRecipeCategoryRegistration registry)
+	{
+		registry.addRecipeCategories(new ImbuingRecipeCategory(registry.getJeiHelpers().getGuiHelper()));
 	}
 
 	@Override
