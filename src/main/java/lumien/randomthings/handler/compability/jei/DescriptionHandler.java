d690cdfdc4497f4fcc33af9f59cb27124622510f
Lumien <marian.kreiser@googlemail.com>
2018-11-20 13:41:34 +0100

    Remove Spectre String Description

--- src/main/java/lumien/randomthings/handler/compability/jei/DescriptionHandler.java
+++ src/main/java/lumien/randomthings/handler/compability/jei/DescriptionHandler.java
@@ -56,7 +56,8 @@ public class DescriptionHandler
 		stackBlackList.add(new ItemStack(ModItems.ingredients, 1, ItemIngredient.INGREDIENT.FLOO_POWDER.id));
 		stackBlackList.add(new ItemStack(ModItems.ingredients, 1, ItemIngredient.INGREDIENT.PLATE_BASE.id));
 		stackBlackList.add(new ItemStack(ModItems.ingredients, 1, ItemIngredient.INGREDIENT.PRECIOUS_EMERALD.id));
-
+		stackBlackList.add(new ItemStack(ModItems.ingredients, 1, ItemIngredient.INGREDIENT.SPECTRE_STRING.id));
+		
 		removeDes(overrideMap, ModBlocks.spectreLeaf, ModBlocks.natureCore, ModBlocks.spectreLog, ModBlocks.spectrePlank, ModBlocks.specialChest, ModBlocks.superLubricentPlatform, ModBlocks.filteredSuperLubricentPlatform);
 
 		
